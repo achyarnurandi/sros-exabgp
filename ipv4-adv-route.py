3283c8b70945970e351bdefe21e143d59e5a9a04
Netcodebynull <kheerand95@gmail.com>
2023-07-27 14:35:09 +0800

    Initiate test of att 28 for BGP ELC IPv4 Unicast

--- ipv4-adv-route.py
+++ ipv4-adv-route.py
@@ -6,8 +6,9 @@ from sys import stdout
 from time import sleep
 
 messages = [
-    'announce route 100.10.0.0/24 next-hop self',
-    'announce route 200.20.0.0/24 next-hop self',
+    'announce attributes origin IGP as-path [65000 29608 3356 29396 29396 29396 39686 44953 ] med 13 community [3356:2 3356:22 3356:100 3356:123 3356:503 3356:2067 29608:30600] next-hop self nlri 93.95.248.0/21',
+    'announce route 100.10.0.0/24 next-hop self as-path [65000 4818 4788 6300 1200] origin IGP attribute [0x1C 0xC0 0x00]',
+    'announce route 200.20.0.0/24 next-hop self as-path [65000 4818 4788 6300 1200] origin IGP attribute [0x1C 0xE0 0x00]',
 ]
 
 sleep(5)
